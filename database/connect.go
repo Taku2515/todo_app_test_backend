1297f017a552096b2914511f10e621cb455f5e5f
Taku2515 <it.program.2357@gmail.com>
2024-09-08 20:47:05 +0900

    create api to create user

--- database/connect.go
+++ database/connect.go
@@ -10,7 +10,7 @@ import (
 var DB *gorm.DB
 var err error
 
-func init() {
+func Init() {
 	dsn := "tester:password@tcp(db:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
--- main.go
+++ main.go
@@ -1,26 +1,18 @@
 package main
 
 import (
-	"net/http"
 
 	"backend/database"
+	"backend/services"
 
 	"github.com/labstack/echo/v4"
 )
 
-func connect(c echo.Context) error {
-	db, _ := database.DB.DB()
-	defer db.Close()
-	err := db.Ping()
-	if err != nil {
-		return c.String(http.StatusInternalServerError, "DB接続失敗しました")
-	} else {
-		return c.String(http.StatusOK, "DB接続しました")
-	}
-}
-
 func main() {
 	e := echo.New()
-	e.GET("/", connect)
+	database.Init()
+
+	e.POST("/create/user", services.NewUser)
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
